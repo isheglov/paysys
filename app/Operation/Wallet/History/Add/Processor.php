dfbd24faf0251bc719edc1eb49dc75afc2a9b827
Ilya Shcheglov <ilia.sheglov@factorymarket.com>
2018-07-23 09:01:03 +0300

    Added processor implementation

--- app/Operation/Wallet/History/Add/Processor.php
+++ app/Operation/Wallet/History/Add/Processor.php
@@ -4,5 +4,12 @@ namespace App\Operation\Wallet\History\Add;
 
 final class Processor implements ProcessorInterface
 {
+    /**
+     * @param $historyDto
+     * @return void
+     */
+    public function process($historyDto)
+    {
 
+    }
 }
